3ee334d49ac8e0d7997dfa08451772496f0dcc6d
herewithme <amaury@beapi.fr>
2012-08-28 19:51:35 +0200

    Fix sample for class usage

--- samples/sample.php
+++ samples/sample.php
@@ -21,7 +21,7 @@ if ($translation_factory -> have_translations()) {
 }
 
 // Manual init view
-$translation_factory = new Bea_MM_Translation_Factory('home');
+$translation_factory = new Bea_MM_Translation_Factory('home', array('blog_id' => 3));
 if ($translation_factory -> have_translations()) {
 	while ($translation_factory -> have_translations()) {
 		$translation_factory -> the_translation();
@@ -34,35 +34,7 @@ if ($translation_factory -> have_translations()) {
 		$translation_factory -> get_permalink();
 		
 		// Translation/Site
-		$translation_factory -> get_site_id();
-		$translation_factory -> get_language_label();
-		$translation_factory -> get_language_code();
-		$translation_factory -> get_home_permalink();
-	}
-}
-
-// Auto init view
-$translation_factory = new Bea_GroupSites_Factory();
-if ($translation_factory -> have_sites()) {
-	while ($translation_factory -> have_sites()) {
-		$translation_factory -> the_site();
-		
-		// Translation/Site
-		$translation_factory -> get_site_id();
-		$translation_factory -> get_language_label();
-		$translation_factory -> get_language_code();
-		$translation_factory -> get_home_permalink();
-	}
-}
-
-// Auto init view
-$translation_factory = new Bea_GroupSites_Factory( $blog_id = 5 );
-if ($translation_factory -> have_sites()) {
-	while ($translation_factory -> have_sites()) {
-		$translation_factory -> the_site();
-		
-		// Translation/Site
-		$translation_factory -> get_site_id();
+		$translation_factory -> get_blog_id();
 		$translation_factory -> get_language_label();
 		$translation_factory -> get_language_code();
 		$translation_factory -> get_home_permalink();
