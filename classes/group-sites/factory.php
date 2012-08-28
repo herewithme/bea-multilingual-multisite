d3d29fec4b9a58353941491a789ebdb30e683781
herewithme <amaury@beapi.fr>
2012-08-28 17:49:13 +0200

    Finalize group sites factory

--- classes/group-sites/factory.php
+++ classes/group-sites/factory.php
@@ -1,36 +1,131 @@
 <?php
 /**
- *  Class for make groups sites factory, allow to get 
+ *  Class for make groups sites factory, allow to get
  *  This class used a global for save group, as WP for CPT or Taxonomies
  */
 class Bea_MM_GroupSites_Factory {
 	/**
 	 * Constructor, do nothing
 	 */
-	function __construct() {
-		$defaults = array('name' => null, 'label' => null, 'sites' => array());
-		$args = wp_parse_args($args, $defaults);
+	public function __construct( ) {
 	}
 
+	/**
+	 * Get group of current blog
+	 * @return array|false
+	 */
+	public function get_current_group( ) {
+		global $wpdb;
+		return self::get_group_by_blog_id( $wpdb->blogid );
+	}
+	
+	/**
+	 * Get group for a blog id specified
+	 * @param  integer $blog_id [description]
+	 * @return array|false
+	 */
+	public static function get_group_by_blog_id( $blog_id = 0 ) {
+		global $groupsites_factory;
+		
+		foreach( $groupsites_factory as $name => $blogs ) {
+			foreach( $blogs as $id => $blog ) {
+				if ( $blog_id == $id ) {
+					return $groupsites_factory[$name];
+				}
+			}
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * [get_group description]
+	 * @param  string $name [description]
+	 * @return array|false
+	 */
+	public static function get_group( $name = '' ) {
+		global $groupsites_factory;
+		
+		if ( empty( $name ) || !isset($groupsites_factory[$name]) ) {
+			return false;
+		}
+		
+		return $groupsites_factory[$name];
+	}
 
-	
-	function get_current() {
+	/**
+	 * Get all group sites registered for this plugin
+	 * @return array
+	 */
+	public static function get_all_groups() {
+		global $groupsites_factory;
+		return $groupsites_factory;
+	}
+
+	/**
+	 * Register new group into factory
+	 * @param  string $name  [description]
+	 * @param  string $label [description]
+	 * @param  array  $blogs [description]
+	 * @return boolean
+	 */
+	public static function register( $name = '', $label = '', $blogs = array() ) {
+		global $groupsites_factory;
+
+		if ( empty( $name ) || empty( $label ) || empty( $blogs ) || !is_array( $blogs ) ) {
+			return false;
+		}
+
+		// Add group into factory
+		$groupsites_factory[$name] = array( );
+		$groupsites_factory[$name]['label'] = $label;
+		$groupsites_factory[$name]['blogs'] = array( );
+
+		// Add blogs object
+		foreach ( $blogs as $blog ) {
+			self::append( $name, $blog );
+		}
 		
+		return true;
 	}
 	
-	function get_registered() {
+	/**
+	 * Allow to add a blog object into factory
+	 * @param  string $name [description]
+	 * @param  array  $blog [description]
+	 * @return boolean
+	 */
+	public static function append( $name = '', $blog = array() ) {
+		global $groupsites_factory;
 		
-	}
-	
-	function get() {
+		if ( empty( $name ) || empty( $blogs ) || !is_array( $blogs ) || !isset($groupsites_factory[$name]) ) {
+			return false;
+		}
 		
-	}
-	
-	function register( $name = '', $label = '', $blogs = array() ) {
+		// Sample: array( 'blog_id' => 3, 'language_code' => 'de_DE', 'public_label' => 'German',   'admin_label' => 'German' ),
+		$site = new Bea_MM_GroupSites_Site( $blog['blog_id'], $blog['language_code'], $blog['public_label'], $blog['admin_label'] );
+		if ( $site->exist( ) ) {
+			$groupsites_factory[$name]['blogs'][$site->get_id()] = $site;
+			return true;
+		}
 		
+		return false;
 	}
-	
-	function deregister() {
-		
+
+	/**
+	 * Remove group site from factory
+	 * @param  string $name [description]
+	 * @return boolean
+	 */
+	public static function deregister( $name = '' ) {
+		global $groupsites_factory;
+
+		if ( isset( $groupsites_factory[$name] ) ) {
+			unset( $groupsites_factory[$name] );
+			return true;
+		}
+
+		return false;
 	}
+
 }
--- classes/group-sites/site.php
+++ classes/group-sites/site.php
@@ -11,39 +11,54 @@ class Bea_MM_GroupSites_Site {
 
 	/**
 	 * Constructor, setup class object
-	 * 
+	 *
 	 * @param integer $blog_id       [description]
 	 * @param string  $language_code [description]
 	 * @param string  $public_label  [description]
 	 * @param string  $admin_label   [description]
 	 */
-	public function __construct($blog_id = 0, $language_code = '', $public_label = '', $admin_label = '') {
-		if ($blog_id == 0 || empty($language_code) || empty($public_label) || empty($admin_label)) {
+	public function __construct( $blog_id = 0, $language_code = '', $public_label = '', $admin_label = '' ) {
+		// All parameters are required
+		if ( $blog_id == 0 || empty( $language_code ) || empty( $public_label ) || empty( $admin_label ) ) {
 			return false;
 		}
-		
-		$this -> obj = new stdClass;
-		$this -> obj -> blog_id = $blog_id;
-		$this -> obj -> language_code = $language_code;
-		$this -> obj -> public_label = $public_label;
-		$this -> obj -> admin_label = $admin_label;
+
+		// Blog exist ?
+		$blog = get_blog_details( $blog_id, false );
+		if ( $blog == false )
+			return false;
+
+		$this->obj = new stdClass;
+		$this->obj->blog_id = $blog_id;
+		$this->obj->language_code = $language_code;
+		$this->obj->public_label = $public_label;
+		$this->obj->admin_label = $admin_label;
+
 		return true;
 	}
-
+	
+	/**
+	 * Test if blog exist and valid
+	 * @return boolean
+	 */
+	public function exist( ) {
+		return is_null($this->obj);
+	}
+	
 	/**
 	 * Get blog ID
 	 * @return integer
 	 */
-	public function get_id() {
-		return $this -> obj -> blog_id;
+	public function get_id( ) {
+		return $this->obj->blog_id;
 	}
 
 	/**
 	 * Get language code, ex: fr_FR or de_DE
 	 * @return string
 	 */
-	public function get_language_code() {
-		return $this -> obj -> language_code;
+	public function get_language_code( ) {
+		return $this->obj->language_code;
 	}
 
 	/**
@@ -53,19 +68,19 @@ class Bea_MM_GroupSites_Site {
 	 */
 	public function get_language_label( $admin = false ) {
 		if ( $admin == true )
-			return $this -> obj -> admin_label;
-		
-		return $this -> obj -> public_label;
+			return $this->obj->admin_label;
+
+		return $this->obj->public_label;
 	}
 
 	/**
 	 * Get blog home url
- 	 * @param  string $path   (optional) Path relative to the home url.
- 	 * @param  string $scheme (optional) Scheme to give the home url context. Currently 'http', 'https', or 'relative'.
+	 * @param  string $path   (optional) Path relative to the home url.
+	 * @param  string $scheme (optional) Scheme to give the home url context. Currently 'http', 'https', or 'relative'.
 	 * @return string
 	 */
 	public function get_permalink( $path = '', $scheme = null ) {
-		return get_home_url( $this -> get_id(), $path, $scheme );
+		return get_home_url( $this->get_id( ), $path, $scheme );
 	}
 
 	/**
@@ -73,8 +88,8 @@ class Bea_MM_GroupSites_Site {
 	 * @param  string $key [description]
 	 * @return mixed
 	 */
-	public function __get($key = '') {
-		return (isset($this -> obj -> $key) ? $this -> obj -> $key : null);
+	public function __get( $key = '' ) {
+		return (isset( $this->obj->$key ) ? $this->obj->$key : null);
 	}
 
 }
