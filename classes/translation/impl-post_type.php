de7eaeaead53072aa19c9df303697ad5c44736e0
herewithme <amaury@beapi.fr>
2012-08-29 17:18:40 +0200

    add test, always return a value

--- classes/translation/impl-post_type.php
+++ classes/translation/impl-post_type.php
@@ -5,18 +5,26 @@ class Bea_MM_Translation_View_PostType implements Bea_MM_Translation_View {
 
 	function __construct( $args = array() ) {
 		$defaults = array( 'blog_id' => 0, 'source_blog_id' => 0, 'post_id' => 0 );
+		
 		$this->obj = (object) wp_parse_args( $args, $defaults );
-
+		
+		// Post ID exist ?
+		if ( $this->obj->post_id == 0 ) {
+			return false;
+		}
+		
 		// Get orginal connection for get group
 		$connexion = new Bea_MM_Connection_Object( 'post_type', $this->obj->source_blog_id, $this->obj->post_id, false );
 		if ( $connexion->exists( ) ) {
 			// If group exist, load connections for this group
 			$factory = new Bea_MM_Connection_Factory( );
 			$factory->load_by_group_id( $connexion->get_group_id( ) );
-
+			
 			// Get translated connection for destination blog id
 			$this->connection = $factory->get_by_blog_id( $this->obj->blog_id );
 		}
+		
+		return true;
 	}
 
 	public function get_blog_id( ) {
