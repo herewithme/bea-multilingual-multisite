fb1d36064920f982b56ef5fd8da7c4643cda1c03
herewithme <amaury@beapi.fr>
2012-08-29 16:35:36 +0200

    rename site by blog

--- classes/translation/interface.php
+++ classes/translation/interface.php
@@ -1,7 +1,7 @@
 <?php
 Interface Bea_MM_Translation_View {
 	public function __construct($args);
-	public function get_site_id();
+	public function get_blog_id();
 	public function get_type();
 	public function get_id();
 	public function get_permalink();
