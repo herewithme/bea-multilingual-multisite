7aaa5a42a31981a2553d46bc537871b6ead8cb9c
herewithme <amaury@beapi.fr>
2012-09-04 09:25:51 +0200

    first pass on metabox for post_type translation

--- classes/admin/post-type.php
+++ classes/admin/post-type.php
@@ -18,7 +18,7 @@ class Bea_MM_Admin_PostType {
 			return false;
 
 		foreach ( get_post_types(array('show_ui' => true), 'names') as $cpt ) {
-			add_meta_box( 'bea-mm', __( 'Translations', 'bea-mm' ), array( __CLASS__, 'metabox' ), $cpt, 'side', 'high' );
+			add_meta_box( 'bea-mm', __( 'Translations', 'bea-mm' ), array( __CLASS__, 'metabox' ), $cpt, 'side', 'high', array('post_type' => $cpt) );
 		}
 
 		return true;
@@ -29,13 +29,44 @@ class Bea_MM_Admin_PostType {
 	 * @param  [type] $object [description]
 	 * @return [type]
 	 */
-	public static function metabox( $object ) {
-		$current_groupsites = Bea_MM_GroupSites_Factory::get_current_group( );
-
+	public static function metabox( $object, $metabox ) {
+		$cptype =$metabox['args']['post_type'];
 		// Always show this nonce field for detect metabox for save
 		wp_nonce_field( 'form-bea-mm', 'bea_mm_noncename' );
-
-		$output = wp_dropdown_pages( array( 'post_type' => $object->post_type, 'selected' => $mydata->$language, 'name' => 'translations[' . $language . ']', 'show_option_none' => ' ', 'sort_column' => 'menu_order, post_title' ) );
+		
+		$output = '';
+		
+		// Loop on translation
+		$translation_factory = new Bea_MM_Translation_Factory('post_type', array('post_id' => $object->ID),  get_current_blog_id());
+		if ($translation_factory -> have_translations()) {
+			while ($translation_factory -> have_translations()) {
+				$translation_factory -> the_translation();
+				
+				// Skip current translation
+				if ( $translation_factory->is_current_translation() ) {
+					continue;
+				}
+				
+				// Get translated ID
+				if ( $translation_factory->translation_exists() ) {
+					$current_id = $translation_factory -> get_translation_id();
+				} else {
+					$current_id = 0;
+				}
+				
+				$output .= '<p>';
+					$output .= '<label for="'.'translations-' . $translation_factory -> get_blog_id().'">'.$translation_factory -> get_language_label( true ).'</label>';
+				
+					switch_to_blog( $translation_factory -> get_blog_id() );
+						$output .= Bea_MM_Plugin::dropdownObjects( 
+							array('post_type' => $cptype, 'sort_column' => 'menu_order, post_title'), 
+							array( 'class' => 'widefat', 'echo' => 0, 'selected' => $current_id, 'name' => 'translations[' . $translation_factory -> get_blog_id() . ']', 'show_option_none' => ' ', 'option_none_value' => 0, 'id' => 'translations-' . $translation_factory -> get_blog_id() ) );
+					restore_current_blog();
+				$output .= '</p>';
+			}
+		}
+		
+		echo $output;
 	}
 
 	/**
@@ -45,9 +76,38 @@ class Bea_MM_Admin_PostType {
 	 * @return [type]
 	 */
 	public static function save_post( $object_id = 0, $object = null ) {
-		if ( (defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE) || wp_is_post_revision( $object_id ) || !isset( $_POST['bea_mm_noncename'] ) || !wp_verify_nonce( $_POST['bea_mm_noncename'], 'form-bea-mm' ) )
+		if ( (defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE) || wp_is_post_revision( $object_id ) || !isset( $_POST['bea_mm_noncename'] ) || !wp_verify_nonce( $_POST['bea_mm_noncename'], 'form-bea-mm' ) ||  !isset($_POST['translations'] ) )
 			return false;
-
+		
+		// Init current factory and remove group
+		$connection_factory = new Bea_MM_Connection_Factory();
+		$connection_factory->load_by_object( 'post_type', get_current_blog_id(), $object_id);
+		$connection_factory->ungroup();
+		
+		// Cast values, expected integer
+		$_POST['translations'] = array_map('intval', $_POST['translations']);
+		
+		// Convert array format for class usage
+		$translations_to_load = array();
+		foreach( $_POST['translations'] as $translation_blog_id => $translation_obj_id ) {
+			if ( $translation_obj_id == 0 ) {
+				continue;
+			}
+			
+			$translations_to_load[] = array( 'blog_id' => $translation_blog_id, 'object_id' => $translation_obj_id );
+		}
+		
+		// Add current object/blog
+		$translations_to_load[] = array( 'blog_id' => get_current_blog_id(), 'object_id' => $object_id );
+		
+		// Init new factory and set group !
+		$connection_factory = new Bea_MM_Connection_Factory();
+		$connection_factory->load( 'post_type', $translations_to_load );
+		
+		// Group if more than one translations !
+		if ( count($translations_to_load) > 1 ) {
+			$connection_factory->group();
+		}
 	}
 
 }
